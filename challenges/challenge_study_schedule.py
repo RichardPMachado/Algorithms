c5ecda4ca0ef0ab8b00eeca997dc90b7260ee37e
RichardPMachado <richardmachado2211@gmail.com>
2023-05-26 14:10:12 -0300

    feat[requisito 1]: completo

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,4 +1,14 @@
 def study_schedule(permanence_period, target_time):
     """Faça o código aqui."""
-    if None in (permanence_period, target_time):
+    # if None in (permanence_period, target_time):
+    #     return None
+    try:
+        result = 0
+        for period in permanence_period:
+            if period[0] <= target_time <= period[1]:
+                result += 1
+        # print(permanence_period)
+        # print(result)
+        return result
+    except TypeError:
         return None
