6cf960a723225405e06c51a7ff27f89264ca1de4
RichardPMachado <richardmachado2211@gmail.com>
2023-05-26 15:01:06 -0300

    feat[requisito 2]: test encrypt_message

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,9 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("euacertei", 1) == "e_ietrecau"
+    assert encrypt_message("euacertei", 10) == "ietrecaue"
+    with pytest.raises(TypeError):
+        encrypt_message(1, "euacertei")
