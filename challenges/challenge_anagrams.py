f0fc58a024dd6cc7808124ee67ce13971099d33a
RichardPMachado <richardmachado2211@gmail.com>
2023-05-26 17:49:40 -0300

    feat[requisito 4]: completo

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,13 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    if (len(first_string) or len(second_string)) == 0:
-        return False
     a = ''.join(sorted(first_string.lower()))
     b = ''.join(sorted(second_string.lower()))
+    if (len(a) or len(a)) == 0:
+        return (a, b, False)
 
     if a == b:
-        return True
+        return (a, b, True)
     else:
-        False
+        return (a, b, False)
+
+# print(is_anagram("pata", ""))
