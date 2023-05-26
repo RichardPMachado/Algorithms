b88d84fea6cc4788a431333a04d7cdb604d842ec
RichardPMachado <richardmachado2211@gmail.com>
2023-05-26 17:19:01 -0300

    feat[requisito 4]: nao consigo executar test

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,11 @@
 def is_anagram(first_string, second_string):
     """Faça o código aqui."""
-    raise NotImplementedError
+    if (len(first_string) or len(second_string)) == 0:
+        return False
+    a = ''.join(sorted(first_string.lower()))
+    b = ''.join(sorted(second_string.lower()))
+
+    if a == b:
+        return True
+    else:
+        False
