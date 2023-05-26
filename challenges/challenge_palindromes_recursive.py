64e3b76bd61a8eedf6d46bc2becb041118e1f8d7
RichardPMachado <richardmachado2211@gmail.com>
2023-05-26 16:33:08 -0300

    feat[requisito 3]: continuo não conseguindo fazer o pytest

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,11 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
     """Faça o código aqui."""
-    try:
-        first_caractere = word[low_index]
-        last_caractere = word[high_index]
-        if low_index < high_index:
-            if first_caractere == last_caractere:
-                return True
-        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
-    except (TypeError, IndexError):
+    first_caractere = word[low_index]
+    last_caractere = word[high_index]
+    if len(word) == 0 or first_caractere != last_caractere:
         return False
+    if low_index > high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
